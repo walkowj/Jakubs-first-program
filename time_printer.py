a674cdf66e35f855dd9436f8108c38569868ff29
WalkowJ <kubax888@gmail.com>
2018-07-01 21:18:29 +1000

    Show time in different time zones. Just copy and paste the code below and if you want to change the time zones with these codes: https://gist.github.com/heyalexej/8bf688fd67d7199be4a1682b3eec7568

---
+++ time_printer.py
@@ -0,0 +1,24 @@
+from datetime import datetime, timedelta
+import pytz
+
+eastern = pytz.timezone('Australia/Sydney')
+loc_dt = eastern.localize(datetime.now() - timedelta(days=3472))
+
+samoa = pytz.timezone('Pacific/Samoa')
+samoa_dt = loc_dt.astimezone(samoa)
+
+warsaw = pytz.timezone('Europe/Warsaw')
+warsaw_dt = loc_dt.astimezone(warsaw)
+
+phili = pytz.timezone('US/Eastern')
+phili_dt = loc_dt.astimezone(phili)
+
+format = '%Y-%m-%d %I:%M:%S %p %A'
+print("Sydney time ziomeczki:")
+print(loc_dt.strftime(format))
+print("Warsaw time:")
+print(warsaw_dt.strftime(format))
+print("Phili time:")
+print(phili_dt.strftime(format))
+print("Samoa time:")
+print(samoa_dt.strftime(format))
